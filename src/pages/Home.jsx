98ad5f44536087db0e3d22817f562a07aca3c91d
Myo Min Thu <myominthu819@gmail.com>
2023-10-18 19:42:00 +0630

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 
 export default function Home() {
   return <div>
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  </div>;
+      <h1 className="text-dark-purple text-3xl">Hello World!</h1>
+    </div>;
 }
