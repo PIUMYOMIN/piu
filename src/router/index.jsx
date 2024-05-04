c0015fb3d053d2b58b0204808c34729714d2c1f8
Myo Min Thu <myominthu819@gmail.com>
2024-05-04 11:09:33 +0630

    updated: home page updated

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -21,7 +21,7 @@ const router = createBrowserRouter([
     element: <UserLayout />,
     children: [
       {
-        path: "/piu",
+        path: "/",
         element: <Home />
       },
       {
