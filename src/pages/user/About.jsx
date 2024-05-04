43c743406236c5e86ff3073928249e9fefe609ac
Myo Min Thu <myominthu819@gmail.com>
2024-05-04 11:15:31 +0630

    updated: home page route

--- src/pages/user/About.jsx
+++ src/pages/user/About.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 
 export default function About() {
-  return (
-    <div>
-      <div className="max-w-7xl mx-auto">About</div>
-    </div>
-  );
+  return <div>
+      <div className="max-w-7xl mx-auto">This is about pages.</div>
+    </div>;
 }
