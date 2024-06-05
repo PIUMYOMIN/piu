5a62970167f8f0cadd48fd7a44933e8854da08fe
Myo Min Thu <myominthu819@gmail.com>
2024-06-05 19:48:17 +0630

    updated

--- src/pages/user/Admission.jsx
+++ src/pages/user/Admission.jsx
@@ -280,12 +280,9 @@ export default function Admission() {
             </div>
             <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
               <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-city">
-                Student ID
+                Student ID (Option)
               </label>
-              <input className={`appearance-none block w-full bg-gray-200 text-gray-700 border ${error.student_id ? "border-red-500" : "border-gray-200"} rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white`} id="grid-city" type="text" placeholder="Student ID" onChange={e => setStudentId(e.target.value)} />
-              {error.student_id && <p className="text-red-500 text-xs italic">
-                  {error.student_id}
-                </p>}
+              <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" id="grid-city" type="text" placeholder="Student ID" onChange={e => setStudentId(e.target.value)} />
             </div>
           </div>
           <div className="flex flex-wrap -mx-3 mb-6">
